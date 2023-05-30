ec945265c33f543ac9d3aa7362a753dc04910cc0
natthaphon-wr <natthaphon.win@gmail.com>
2023-05-30 17:29:35 +0700

    redundancy of summary

--- eval_src/avg_redun.py
+++ eval_src/avg_redun.py
@@ -8,32 +8,32 @@ bbc_path = r"raw_data\finance_preprocessed_final\BBC"
 bloomberg_path = r"raw_data\finance_preprocessed_final\Bloomberg"
 eest_path = r"raw_data\finance_preprocessed_final\eest"
 
-# CNN dataset
-mean_cnn, count_cnn, df_cnn = open_src_redun(cnn_path)
-print("Total CNN Documents: ", count_cnn)
-print(mean_cnn)
-df_cnn.to_csv(r"eval_src\result/cnn_redun.csv", sep=',', index=False)
+# # CNN dataset
+# mean_cnn, count_cnn, df_cnn = open_src_redun(cnn_path, False)
+# print("Total CNN Documents: ", count_cnn)
+# print(mean_cnn)
+# df_cnn.to_csv(r"eval_src\result/cnn_summ_redun.csv", sep=',', index=False)
 
 # DM dataset
-mean_dm, count_dm, df_dm = open_src_redun(dm_path)
-print("Total BBC Documents: ", count_dm)
+mean_dm, count_dm, df_dm = open_src_redun(dm_path, False)
+print("Total DM Documents: ", count_dm)
 print(mean_dm)
-df_dm.to_csv(r"eval_src\result/dm_redun.csv", sep=',', index=False)
+df_dm.to_csv(r"eval_src\result/dm_summ_redun.csv", sep=',', index=False)
 
-# BBC dataset
-mean_bbc, count_bbc, df_bbc = open_src_redun(bbc_path)
-print("Total BBC Documents: ", count_bbc)
-print(mean_bbc)
-df_bbc.to_csv(r"eval_src\result/bbc_redun.csv", sep=',', index=False)
+# # BBC dataset
+# mean_bbc, count_bbc, df_bbc = open_src_redun(bbc_path, False)
+# print("Total BBC Documents: ", count_bbc)
+# print(mean_bbc)
+# df_bbc.to_csv(r"eval_src\result/bbc_summ_redun.csv", sep=',', index=False)
 
-# Bloomberg dataset
-mean_bloom, count_bloom, df_bloom = open_src_redun(bloomberg_path)
-print("Total Bloomberg Documents: ", count_bloom)
-print(mean_bloom)
-df_bloom.to_csv(r"eval_src\result/bloomberg_redun.csv", sep=',', index=False)
+# # Bloomberg dataset
+# mean_bloom, count_bloom, df_bloom = open_src_redun(bloomberg_path, False)
+# print("Total Bloomberg Documents: ", count_bloom)
+# print(mean_bloom)
+# df_bloom.to_csv(r"eval_src\result/bloomberg_summ_redun.csv", sep=',', index=False)
 
-# EEST dataset
-mean_eest, count_eest, df_eest = open_src_redun(eest_path)
-print("Total EEST Documents: ", count_eest)
-print(mean_eest)
-df_eest.to_csv(r"eval_src\result/eest_redun.csv", sep=',', index=False)
+# # EEST dataset
+# mean_eest, count_eest, df_eest = open_src_redun(eest_path, False)
+# print("Total EEST Documents: ", count_eest)
+# print(mean_eest)
+# df_eest.to_csv(r"eval_src\result/eest_summ_redun.csv", sep=',', index=False)
--- eval_src/read_src.py
+++ eval_src/read_src.py
@@ -36,7 +36,7 @@ def open_src_count(path):
   return result_mean, len(filelist), result_all
 
 # Redundancy Metrics
-def open_src_redun(path):
+def open_src_redun(path, doc=True):
   filelist = os.listdir(path)
   result_all = pd.DataFrame(columns = ['unique_unigram',
                                       'unique_bigram',
@@ -49,15 +49,24 @@ def open_src_redun(path):
       with open(file_path, 'r', encoding="utf8") as f:
         text = f.read()
         text = text.lower()
-        src = text.split('@highlight')[0]
+        txt = text.split('@highlight')[0]
+
+        if not doc:
+          # print(len(text.split('@highlight')))
+          summary = ''
+          for i in range(1, len(text.split('@highlight'))):
+            s = text.split('@highlight')[i]
+            summary = summary + s
+          # print(summary)
+          txt = summary
 
         # Unique ngrams ratio
-        uniq_unigram = unique_ngrams_ratio(src, 1)
-        uniq_bigram = unique_ngrams_ratio(src, 2)
-        uniq_trigram = unique_ngrams_ratio(src, 3)
+        uniq_unigram = unique_ngrams_ratio(txt, 1)
+        uniq_bigram = unique_ngrams_ratio(txt, 2)
+        uniq_trigram = unique_ngrams_ratio(txt, 3)
 
         #NID
-        nid = normal_inver_diver(src)
+        nid = normal_inver_diver(txt)
 
         result_doc = pd.DataFrame([[uniq_unigram, uniq_bigram, uniq_trigram, nid]],
                                   columns=['unique_unigram',
